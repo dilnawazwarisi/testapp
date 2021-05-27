f7096843961b9502e5fa65bbc02c748588438761
dilnawazwarisi <dilnawaz07@gmail.com>
2021-05-28 00:08:02 +0530

    Adding new code

--- test_service.py
+++ test_service.py
@@ -5,7 +5,7 @@ def test_get_message():
     resp = get_message('jabba')
     assert resp == 'Hi jabba'
 
-    
+
 def test_get_message2():
     resp = get_message('jabbaa')
     assert resp == 'Hi jabbaa'
